a10c714e2e4f4e857c34050493cfd6228ce22bed
ECLIPSE170790 <eclipse170790@mail.ru>
2021-09-04 20:42:03 +0600

    new test

---
+++ src/test/java/ru/safron/DemoQATest.java
@@ -0,0 +1,36 @@
+package ru.safron;
+
+import org.junit.jupiter.api.*;
+
+public class DemoQATest {
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Before all!\n");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("After all!\n");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("Before\n");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("After\n");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("Blyaha-muha\n");
+    }
+
+    @Test
+    void SecondTest() {
+        System.out.println("Second\n");
+    }
+}
