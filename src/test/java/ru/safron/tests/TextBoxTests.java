79c044990f01aa8789823476bf39e20c8e35e664
ECLIPSE170790 <eclipse170790@mail.ru>
2021-09-05 00:57:08 +0600

    add TextBoxTests

---
+++ src/test/java/ru/safron/tests/TextBoxTests.java
@@ -0,0 +1,35 @@
+package ru.safron.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void fillFormTest() {
+        String permamentAddress = "some street 1";
+
+
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("Eclipse");
+        $("#userEmail").setValue("Eclipse@mail.ru");
+        $("#currentAddress").setValue("Qwe");
+        $("#permanentAddress").setValue(permamentAddress);
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text("Eclipse"));
+        $("#output #email").shouldHave(text("Eclipse@mail.ru"));
+        $("#output #currentAddress").shouldHave(text("Qwe"));
+        $("#output #permanentAddress").shouldHave(text(permamentAddress));
+    }
+}
