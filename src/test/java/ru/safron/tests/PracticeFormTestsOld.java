4c82bf1206b2bb1a96884cdaa6e1845b2208c02a
ECLIPSE170790 <eclipse170790@mail.ru>
2021-11-20 18:34:34 +0600

    Updated TestBase

--- src/test/java/ru/safron/tests/PracticeFormTestsOld.java
+++ src/test/java/ru/safron/tests/PracticeFormTestsOld.java
@@ -2,6 +2,7 @@ package ru.safron.tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -15,6 +16,7 @@ public class PracticeFormTestsOld {
     }
 
     @Test
+    @Disabled
     void FillPracticeForm(){
 
         open("https://demoqa.com/automation-practice-form");
--- src/test/java/ru/safron/tests/TestBase.java
+++ src/test/java/ru/safron/tests/TestBase.java
@@ -2,6 +2,8 @@ package ru.safron.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -10,6 +12,7 @@ public class TestBase {
     @BeforeAll
     static void beforeAll(){
         Configuration.startMaximized = true;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterAll
