15df240fa7e4c4d52ee65a8073f24760a2e4d99a
ECLIPSE170790 <eclipse170790@mail.ru>
2021-11-20 21:02:05 +0600

    Added Attach into TestBase

--- src/test/java/ru/safron/tests/TestBase.java
+++ src/test/java/ru/safron/tests/TestBase.java
@@ -3,8 +3,10 @@ package ru.safron.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -22,6 +24,14 @@ public class TestBase {
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
     @AfterAll
