2012470c78dfb374a09c14ccea3e758bdfdf5bba
ECLIPSE170790 <eclipse170790@mail.ru>
2021-11-20 20:01:48 +0600

    Added Selenoid

--- src/test/java/ru/safron/tests/TestBase.java
+++ src/test/java/ru/safron/tests/TestBase.java
@@ -6,13 +6,21 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll(){
-        Configuration.startMaximized = true;
+
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
