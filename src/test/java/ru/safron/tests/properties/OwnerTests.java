b3f48beba6069b4502ce8daac8f3d9c60df3d8c7
ECLIPSE170790 <eclipse170790@mail.ru>
2021-11-27 16:30:19 +0600

    Added CredentialsConfig, updated gitignore, OwnerTests

--- src/test/java/ru/safron/tests/properties/OwnerTests.java
+++ src/test/java/ru/safron/tests/properties/OwnerTests.java
@@ -1,5 +1,7 @@
 package ru.safron.tests.properties;
 
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
